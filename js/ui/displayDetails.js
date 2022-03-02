6dd4077bbcf0ce4671a9777bf855d4b8d7aa1152
Tiffany Utvaer Gasser <tiffany.u.gasser@gmail.com>
2022-03-02 10:14:44 +0100

    Styling added

--- js/ui/displayDetails.js
+++ js/ui/displayDetails.js
@@ -4,12 +4,16 @@ const product = document.querySelector(".product-detail");
 export function displayDetails(details) {
   document.title = `${details.title}`;
   page.innerHTML = `${details.title}`;
-  product.innerHTML = `<div>
-                        <h1>${details.title}</h1>
-                        <img src="${details.image.formats.large.url}" class="card-img-top" alt="${details.image_alt}">
-                        <p>USD ${details.price}</h6>
-                        <p>Brand ${details.brand}</p>
-                        <p>${details.description}</p>
-                        <button class="btn btn-primary add-bag-btn">Add to Bag</button>
-                       </div> `;
+  product.innerHTML = `<div class="row">
+                        <div class="col-xs-12 col-sm-6">
+                          <img src="${details.image.formats.large.url}" class="card-img-top details-img" alt="${details.image_alt}">
+                        </div>
+                        <div class="col-xs-12 col-sm-6">
+                          <h1>${details.title}</h1>
+                          <p>${details.brand}</p>
+                          <p>${details.description}</p>
+                          <h6 class="card-subtitle mb-2 text-muted">USD ${details.price}</h6>
+                          <button class="btn btn-primary add-bag-btn">Add to Bag</button>
+                        </div>
+                      </div class="row">`;
 }
