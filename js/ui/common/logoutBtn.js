61d42dfc6dd8ff7f7d7266b154249f6869884a13
Tiffany Utvaer Gasser <tiffany.u.gasser@gmail.com>
2022-03-05 23:14:00 +0100

    gitignore node modules and local storage fixes added

--- js/ui/common/logoutBtn.js
+++ js/ui/common/logoutBtn.js
@@ -6,7 +6,8 @@ export default function logoutBtn() {
     logout.onclick = function () {
       const doLogout = confirm("Are you sure you want to log out?");
       if (doLogout) {
-        clearStorage();
+        localStorage.removeItem("user");
+        localStorage.removeItem("token");
         location.href = "/";
       }
     };
